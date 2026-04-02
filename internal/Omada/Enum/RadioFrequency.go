9cead3613c52cfff8603d43f7f2fcc7ad3b863db
Comic-Tom <89974115+Comic-Tom@users.noreply.github.com>
2026-04-02 11:02:50 +1100

    Update RadioFrequency.go - Added 6hgz wifi band

--- internal/Omada/Enum/RadioFrequency.go
+++ internal/Omada/Enum/RadioFrequency.go
@@ -5,6 +5,7 @@ type RadioFrequency int8
 const (
 	RadioFrequency_2_4_Ghz RadioFrequency = 0
 	RadioFrequency_5_0_Ghz RadioFrequency = 1
+	RadioFrequency_6_0_Ghz RadioFrequency = 2
 )
 
 func (rf RadioFrequency) String() string {
@@ -13,6 +14,8 @@ func (rf RadioFrequency) String() string {
 		return "2.4 GHz"
 	case RadioFrequency_5_0_Ghz:
 		return "5.0 GHz"
+	case RadioFrequency_6_0_Ghz:
+		return "6.0 GHz"
 	default:
 		return "invalid"
 	}
